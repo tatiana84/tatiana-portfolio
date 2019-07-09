917843bcbfe760e0935a34def322816fa471f5af
tatiana84 <tantamuntean2018@gmail.com>
2019-07-09 13:05:10 +0400

    modul2

--- src/components/app.js
+++ src/components/app.js
@@ -1,13 +1,41 @@
 import React, { Component } from 'react';
 import moment from "moment";
+import{
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+
+
+import NavigationContainer from "./navigation/navigation-container";
+import Home from "./pages/home";
+import About from "./pages/about";
+import Contact from "./pages/contact";
+import Blog from "./pages/blog";
+import PortfolioDetail from "./portfolio/portfolio-detail";
+import NoMatch from "./pages/no-match";
+
 export default class App extends Component {
   render() {
     return (
       <div className='app'>
-        <h1>Tatiana Muntean Portfolio</h1>
-        <div>
-          {moment().format('MMMM Do YYYY, h:mm:ss a')}
-        </div>
+        <Router>
+          <div>
+            <h1>Tatiana Muntean Portfolio</h1>
+            <div>
+            {moment().format('MMMM Do YYYY, h:mm:ss a')}
+            </div>
+          <NavigationContainer/>
+          <Switch>
+            <Route exact path="/" component={Home}/>
+            <Route path="/About-me" component={About} />
+            <Route path="/Contact" component={Contact} />
+            <Route path="/Blog" component={Blog} />
+            <Route exact path="/portfolio/:slug" component={PortfolioDetail}/>
+            <Route component={NoMatch}/>
+          </Switch>
+          </div>
+        </Router>
       </div>
     );
   }
